4eee73440d973594af3188e4edd1a50b62d9d99d
Tien <viettienha98@gmail.com>
2020-10-08 17:08:05 +0700

    Deploy NestJs on Cloud function

--- backend/src/model/books/books.service.ts
+++ backend/src/model/books/books.service.ts
@@ -26,11 +26,13 @@ export class BooksService {
             data.key = doc.id;
             result.push(data);
         });
+        console.log("Get all books");
         return result;
     }
 
     async findById(id: string): Promise<BooksDTO> {
         const snapshot = await this.firebase.db.collection('books').doc(id).get();
+        console.log("Get book by id: ", id);
         return this.preprocessData(snapshot.data());
     }
 
@@ -38,7 +40,19 @@ export class BooksService {
         bookDTO.date = new Date(bookDTO.date)
         console.log(bookDTO);
         const doc = await this.firebase.db.collection('books').add(bookDTO);
-        console.log('Added document with ID: ', doc.id);
+        console.log("Create book by id: ", doc.id);
+        return { success: true, id: doc.id };
+    }
+
+    async editById(id: string, bookDTO: BooksDTO): Promise<any> {
+        await this.firebase.db.collection('books').doc(id).set(bookDTO);
+        console.log("Edit book by id: ", id);
+        return { success: true };
+    }
+
+    async deleteById(id: string): Promise<any> {
+        await this.firebase.db.collection('books').doc(id).delete();
+        console.log("Delete book by id: ", id);
         return { success: true };
     }
 
